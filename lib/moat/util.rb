58b4bc01977888f5ddf7f15343514eb513721837
paul.cantrell <paul.cantrell@f471d5c0-7d2a-0410-a9f7-fd84b96402ba>
2007-03-01 22:55:48 +0000

    Trying out a two-space indent at RUMmers' suggestion. Maybe I'll learn to like it.

--- lib/moat/util.rb
+++ lib/moat/util.rb
@@ -1,13 +1,13 @@
 module Moat
-    
-    # Resolves hostnames to numeric IPs. If the given host is already a numberic IP,
-    # this is a noop.
-    
-    def self.resolve_ip(host)
-        raise "Empty host; your regexp may not match the auth log format" if host =~ /^\s*$/
-        return host if host =~ /^((\d+)\.(\d+)\.(\d+)\.(\d+)|([\dA-F:]+))$/i
-        ip = Socket.getaddrinfo(host, 0)[0][3]
-        raise "Unable to resolve ip for #{host}" unless ip
-        return ip
-    end    
+  
+  # Resolves hostnames to numeric IPs. If the given host is already a numberic IP,
+  # this is a noop.
+  
+  def self.resolve_ip(host)
+    raise "Empty host; your regexp may not match the auth log format" if host =~ /^\s*$/
+    return host if host =~ /^((\d+)\.(\d+)\.(\d+)\.(\d+)|([\dA-F:]+))$/i
+    ip = Socket.getaddrinfo(host, 0)[0][3]
+    raise "Unable to resolve ip for #{host}" unless ip
+    return ip
+  end  
 end
